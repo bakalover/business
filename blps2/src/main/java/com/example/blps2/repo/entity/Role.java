87b44b8c16f1b111ec34338f45a01d9c5589795d
bakalover <kostya@shprenger.com>
2024-03-20 11:41:54 +0300

    NO privileges

--- blps2/src/main/java/com/example/blps2/repo/entity/Role.java
+++ blps2/src/main/java/com/example/blps2/repo/entity/Role.java
@@ -29,8 +29,4 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private Collection<UserDao> users;
-
-    // @ManyToMany
-    // @JoinTable(name = "roles_privileges", joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "privilege_id", referencedColumnName = "id"))
-    // private Collection<Privilege> privileges;
 }
